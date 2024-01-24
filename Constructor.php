374d80a522a6be3d84acbf75527b79a1543f2de2
liaqat-programmer <liaqat122012@gmail.com>
2024-01-24 18:50:20 +0500

    Constructor

---
+++ Constructor.php
@@ -0,0 +1,12 @@
+<?php
+//constructor: which automatically call when make class object
+class Constructor{
+    public function __construct(){
+        echo "This is Constructor";
+    }
+}
+
+$c1 = new Constructor();
+
+
+?>
