24ec7faa8a4dc6c375d072ceb3defcf09ea66738
liaqat-programmer <liaqat122012@gmail.com>
2024-01-24 18:56:18 +0500

    Inheretance in php

---
+++ Inheretance.php
@@ -0,0 +1,26 @@
+<?php
+
+class Base{
+    public $name;
+    public $age;
+
+    function __construct($n, $a){
+        $this->name  = $n;
+        $this->age = $a;
+    }
+
+    function Info(){
+        echo "My Name Is :".$this->name."<br>";
+        echo "My Age Is :".$this->age;
+    }
+}
+
+class Child extends Base{
+
+}
+
+$ch1 = new Child("Liaqat Ali", 20);
+
+$ch1->Info();
+
+?>
